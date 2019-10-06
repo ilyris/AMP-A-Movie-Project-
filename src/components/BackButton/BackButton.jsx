8d68f55280d89080bc41e9106ad28c6d3fea199e
Dylan Sieren <dylan.sieren01@gmail.com>
2019-10-05 21:20:44 -0500

    added Go Back text for the back button to let users know what the purpose of the button is

--- src/components/BackButton/BackButton.jsx
+++ src/components/BackButton/BackButton.jsx
@@ -5,7 +5,7 @@ import { faArrowLeft} from "@fortawesome/free-solid-svg-icons";
 
 const BackButton = ({handleBackButton}) => {
     return(
-        <div className="backButton" ><FontAwesomeIcon icon={faArrowLeft} onClick={handleBackButton}/></div>
+        <div className="backButton"><p onClick={handleBackButton}><FontAwesomeIcon icon={faArrowLeft} /><span>Go Back</span></p></div>
     );
 }
 export default BackButton;
